15d8d920284b1549f81d3592ae25c1a544cd4248
ninimimm <nikitachernov89221156246@yandex.ru>
2023-10-21 23:45:24 +0500

    i j

--- Server/mainServer.py
+++ Server/mainServer.py
@@ -61,8 +61,8 @@ if __name__ == '__main__':
         if len(clients) == 1:
             game.players_ip[ip_save] = ["white", True]
             game.players_ip[address[0]] = ["black", False]
-            data = [f"{game.dict_cages[(i, j)].figure.color}_{game.dict_cages[(i, j)].figure.name}{game.dict_cages[(i, j)].figure.index}"
-                    if game.dict_cages[(i, j)].figure is not None else "None" for i in range(8) for j in range(8)]
+            data = [f"{game.dict_cages[(j, i)].figure.color}_{game.dict_cages[(j, i)].figure.name}{game.dict_cages[(j, i)].figure.index}"
+                    if game.dict_cages[(j, i)].figure is not None else "None" for i in range(8) for j in range(8)]
             print(data)
 
             response = game.get_possible_moves(data, "white")
