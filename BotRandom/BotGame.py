6dd5bb2223f0ae68ff873117c8905fdcc2781b03
ninimimm <nikitachernov89221156246@yandex.ru>
2023-10-22 18:54:40 +0500

    fix

--- BotRandom/BotGame.py
+++ BotRandom/BotGame.py
@@ -138,48 +138,6 @@ class BotGame:
                         self.shared_data.copy_field = copy.deepcopy(copy_dict_2)
                 self.shared_data.copy_field = copy.deepcopy(copy_dict_1)
         sort = sorted(variants.items(), reverse=True)
-        print(sort)
-        best_variants = []
-        best_variants.append(sort[0])
-
-        max_weight = -9999
-        for k in range(1):
-            for i in range(len(best_variants[k][1])):
-                self.shared_data.copy_field = copy.deepcopy(copy_dict_1)
-                self.shared_data.copy_field[best_variants[k][1][i][1][0]][best_variants[k][1][i][1][1]] = self.shared_data.copy_field[best_variants[k][1][i][0][0]][best_variants[k][1][i][0][1]]
-                self.shared_data.copy_field[best_variants[k][1][i][0][0]][best_variants[k][1][i][0][1]] = "None"
-                self.shared_data.copy_field[best_variants[k][1][i][3][0]][best_variants[k][1][i][3][1]] = self.shared_data.copy_field[best_variants[k][1][i][2][0]][best_variants[k][1][i][2][1]]
-                self.shared_data.copy_field[best_variants[k][1][i][2][0]][best_variants[k][1][i][2][1]] = "None"
-                self.shared_data.copy_field[best_variants[k][1][i][5][0]][best_variants[k][1][i][5][1]] = self.shared_data.copy_field[best_variants[k][1][i][4][0]][best_variants[k][1][i][4][1]]
-                self.shared_data.copy_field[best_variants[k][1][i][4][0]][best_variants[k][1][i][4][1]] = "None"
-                self.send_color = self.color
-                while not self.shared_data.can_use:
-                    continue
-                self.shared_data.can_use = False
-                max_four_weight = -9999
-                for enemy_key in self.shared_data.game_dict:
-                    for enemy_coordinate in self.shared_data.game_dict[enemy_key]:
-                        enemy_color = self.color
-                        four_weight = 0
-                        eval_coord = (7 - enemy_coordinate[1], enemy_coordinate[0]) if self.color == "black" else (enemy_coordinate[1], enemy_coordinate[0])
-                        if enemy_color in self.shared_data.copy_field[enemy_coordinate[0]][enemy_coordinate[1]]:
-                            four_weight += self.values[self.shared_data.copy_field[enemy_coordinate[0]]
-                                                       [enemy_coordinate[1]].split("_")[1][:-1]]
-                        four_weight += self.evals[self.shared_data.copy_field[enemy_key[0]][enemy_key[1]].
-                                                  split("_")[1][:-1]][eval_coord[0]][eval_coord[1]]
-                        if four_weight > max_four_weight:
-                            max_four_weight = four_weight
-                            keys = [(enemy_key, enemy_coordinate)]
-                        elif four_weight == max_four_weight:
-                            keys.append((enemy_key, enemy_coordinate))
-                if first_weight - max_four_weight in variants:
-                    num = random.randint(0, len(keys) - 1)
-                    variants[first_weight - max_four_weight].append([key, coordinate, keys[num][0], keys[num][1]])
-                else:
-                    num = random.randint(0, len(keys) - 1)
-                    variants[first_weight - max_four_weight] = [[key, coordinate, keys[num][0], keys[num][1]]]
-                self.shared_data.copy_field = copy.deepcopy(copy_dict_1)
-        sort = sorted(variants.items(), reverse=True)
         best_variants = []
         best_variants.append(sort[0])
         try:
