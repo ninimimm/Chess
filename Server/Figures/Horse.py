a6d338f7c61755c23b5f3764168ff02436f9aca9
ninimimm <nikitachernov89221156246@yandex.ru>
2023-10-26 00:51:54 +0500

    print dict_cages horse

--- Server/Figures/Horse.py
+++ Server/Figures/Horse.py
@@ -32,6 +32,7 @@ class Horse:
         possible = set()
         for move in possible_moves:
             if 0 <= move[0] < 8 and 0 <= move[1] < 8:
+                print(dict_cages)
                 target_cage = dict_cages[move]
                 if target_cage.figure is None or target_cage.figure.color != color:
                     possible.add(move)
