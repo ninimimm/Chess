970dbbb1fde010ba8b8542e15b1b2f3c9d6a71d5
ninimimm <nikitachernov89221156246@yandex.ru>
2023-10-27 21:37:43 +0500

    fix import

--- Server/Game.py
+++ Server/Game.py
@@ -1,12 +1,12 @@
-from Server.Cage import Cage
+from Cage import Cage
 from Server.Figures.Pawn import Pawn
 from Server.Figures.Rook import Rook
 from Server.Figures.Horse import Horse
 from Server.Figures.Elephant import Elephant
 from Server.Figures.Queen import Queen
 from Server.Figures.King import King
-from Server.MovesFigures import MoveFigures
-from Server.Player import Player
+from MovesFigures import MoveFigures
+from Player import Player
 
 
 class Game:
